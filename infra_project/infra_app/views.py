a967d6d7954688571ccb7aae30c1a3839d9ace53
ArslanYadov <arsyy90@gmail.com>
2022-11-23 22:09:03 +0300

    testing main page

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    return HttpResponse(
+        '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1">На вторую страницу</a>'
+    )
 
 
 def second_page(request):
