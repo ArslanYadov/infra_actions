ddb5e3dfa785338967bb1dc9889bb0158e115f2b
ArslanYadov <arsyy90@gmail.com>
2022-11-23 21:42:06 +0300

    allow all host in settings; testing main page

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 
 def second_page(request):
